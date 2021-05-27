5ce463927180336d34181ab2fb7f48713d53e672
Inox Danil <inoxdanil@gmail.com>
2021-05-27 16:30:01 +0600

    создан файл для миграции

--- inoxdata/orders/models.py
+++ inoxdata/orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Orders(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    otk = models.BooleanField()
+    part = models.ForeignKey('Parts', on_delete=models.PROTECT)
+
+
+class Parts(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.IntegerField(max_length=255)
