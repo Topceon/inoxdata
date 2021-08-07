9a0c9689fdd23d216a0d64515907ee40dc093506
Inox Danil <inoxdanil@gmail.com>
2021-08-07 12:16:01 +0600

    кнопка копирования работает

--- inoxdata/orders/static/orders/js/ready.js
+++ inoxdata/orders/static/orders/js/ready.js
@@ -5,16 +5,13 @@ $(document).ready(function(){
     })
 })
 
-function copyFunction() {
-  /* Get the text field */
-  var copyText = document.getElementById("for_copy");
+var text = document.getElementById("inputText");
 
-  /* Select the text field */
-  copyText.select();
+/* return button to variable btn */
+var btn = document.getElementById("copyText");
 
-  /* Copy the text inside the text field */
+/* call function on button click */
+btn.onclick = function() {
+  text.select();
   document.execCommand("copy");
-
-  /* Alert the copied text */
-//  alert("Copied the text: " + copyText.value);
 }
